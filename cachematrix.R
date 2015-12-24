fb1918555e82635cb2929d76c821e1d51677f3a2
blueved verisk <vincent.ferre@blueved.com>
2015-12-24 14:34:15 -0600

    first pass

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m<-NULL
+	## set function
+	set<-function(y){
+		x<<-y       ## access the parent environment variable 'x'
+		m<<-NULL	## access the parent environment variable 'm'
+	}
+	## source matrix accessor
+	get<-function() x
+	## calculates the inverse of the given matrix
+	setmatrix<-function(solve) m<<- solve
+	## inverse matrix accessor
+	getmatrix<-function() m
+	## returns a list
+	list(set=set, get=get,
+		setmatrix=setmatrix,
+		getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+	## check if there is cached value for this matrix
+    m<-x$getmatrix()
+    if(!is.null(m)){
+      message("*** getting cached data")
+      return(m)
+    }
+	## no cached value:
+    matrix <- x$get() 		## get the matrix to invert
+    m<-solve(matrix, ...)	## invert it
+    x$setmatrix(m)			## cache the value 
+    return(m)				## return the inverted matrix
 }
+
